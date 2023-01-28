d8bde770e410930bc6a4d1a26a98fcce348fffa7
Chamas111 <ahmad.chamas87@gmail.com>
2023-01-28 14:11:29 +0100

    init commit

---
+++ src/TodoInput.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TodoInput() {
+  return (
+    <div>
+      <h1> Todo from todoinput</h1>
+    </div>
+  );
+}
---
+++ src/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Item from "./TodoItem";
+
+export default function TodoList() {
+  return (
+    <div>
+      <h1>hello from Todolist</h1>
+      <Item />
+    </div>
+  );
+}
